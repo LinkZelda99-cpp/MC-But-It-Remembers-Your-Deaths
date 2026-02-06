4b1a0681511ca100b8b304f4a24b8763332d9a7b
Austin McDonald <austinmcdonald2011@gmail.com>
2026-02-05 20:49:27 -0500

    INIT

---
+++ src/main/java/com/austin/mcbutitremembersyourdeaths/MCRemembers.java
@@ -0,0 +1,12 @@
+
+package com.austin.mcbutitremembersyourdeaths;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("mcbutitremembersyourdeaths")
+public class MCRemembers {
+    public MCRemembers() {
+        System.out.println("MC But It Remembers Your Deaths loaded!");
+    }
+}
+
